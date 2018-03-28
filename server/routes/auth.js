eb203b29963a634ceb0618c72dca5a423449ae66
mutoe <mutoe@foxmail.com>
2018-03-29 03:29:01 +0800

    fix: router post without payload condition

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,13 +4,16 @@ const Router = require('koa-router')
 const router = new Router()
 
 // 注册
-router.post('register', '/user', (ctx, next) => {
-  ctx.body = ctx.request.body
+router.post('/user', (ctx, next) => {
+  const payload = ctx.request.body
+  if (Object.keys(payload).length === 0) ctx.throw(400)
+  ctx.body = payload
 })
 
 // 登陆
-router.get('login', '/user/:user_id', (ctx, next) => {
-  ctx.body = ctx.request.body
+router.get('/user/:user_id', (ctx, next) => {
+  if (!(ctx.params.user_id > 0)) ctx.throw(400)
+  ctx.body = { user_id: ctx.params.user_id }
 })
 
 module.exports = router
