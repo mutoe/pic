6c220b3ab584e01c785bc1320da1b9b136395aa8
mutoe <mutoe@foxmail.com>
2018-03-30 14:12:27 +0800

    fix: header will be override when call ctx.throw()
    because of koajs/koa#1127

--- server/app/middlewares/index.js
+++ server/app/middlewares/index.js
@@ -13,7 +13,16 @@ module.exports = app => {
 
   // others
   app.use(async (ctx, next) => {
-    await next()
-    ctx.set('X-Powered-By', 'Koa2')
+    try {
+      await next()
+    } catch (err) {
+      ctx.set('X-Powered-By', 'Koa2')
+      ctx.status = err.status
+      // ctx.message = err.message
+      ctx.body = {
+        message: err.message,
+        payload: err.payload,
+      }
+    }
   })
 }
