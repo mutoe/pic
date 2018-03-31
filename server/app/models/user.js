a6ec6ee15a9f08b03b4ce1d320bec0bb202aa232
mutoe <mutoe@foxmail.com>
2018-03-31 09:27:11 +0800

    feat: add a field update_time in user schema

--- server/app/models/user.js
+++ server/app/models/user.js
@@ -6,6 +6,7 @@ const UserSchema = new mongoose.Schema({
   password: { type: String, required: true },
   username: { type: String, trim: true, unique: true },
   create_time: { type: Date, default: Date.now },
+  update_time: { type: Date, default: Date.now },
 })
 
 // 使用 setter 将用户输入的明文密码 sha1 之后存储
@@ -13,6 +14,12 @@ UserSchema.path('password').set(function (v) {
   let shasum = crypto.createHash('sha1')
   shasum.update(v)
   return shasum.digest('hex')
+})
+
+// 使用 middleware 每次保存都记录一下最后更新时间
+UserSchema.pre('save', function (next) {
+  this.update_time = Date.now()
+  next()
 })
 
 const UserModel = mongoose.model('User', UserSchema)
