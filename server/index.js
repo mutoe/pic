3db3c7f1eb4197ee6e76348788a67a5d691f84a0
mutoe <mutoe@foxmail.com>
2018-10-20 14:06:38 +0800

    chore: abstract out configuration file

--- server/index.js
+++ server/index.js
@@ -1,25 +1,29 @@
+// 加载 dotenv 配置信息
+require('dotenv').config()
+
+// 连接 mysql
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'pic',
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USERNAME || 'root',
+  password: process.env.MYSQL_PASSWORD || '',
+  database: process.env.MYSQL_DATABASE || 'pic',
 })
-
-const port = process.env.APP_SERVER_LISTEN_PORT || 3000
 
 connection.connect(err => {
   if (err) return console.error(err)
   console.log('mysql is connected successfully!')
 })
 
+// 创建 koa 实例
 const Koa = require('koa')
 const app = new Koa()
+const port = process.env.APP_SERVER_LISTEN_PORT || 3000
 
 app.use(ctx => {
   ctx.body = 'Hello TimeImage'
 })
 
-app.listen(3000, () => {
-  console.log(`Koa is running in port 3000 successfully!`)
+app.listen(port, () => {
+  console.log(`Koa is running in port ${port} successfully!`)
 })
