5791797ca14e2bac19505da9b9eb779d8065a72e
mutoe <mutoe@foxmail.com>
2018-10-27 10:29:29 +0800

    chore: use koa router middleware

--- server/index.js
+++ server/index.js
@@ -3,13 +3,25 @@ require('dotenv').config()
 
 // 创建 koa 实例
 const Koa = require('koa')
+const Router = require('koa-router')
+
 const app = new Koa()
-const port = process.env.APP_SERVER_LISTEN_PORT || 3000
+const router = new Router()
 
-app.use(ctx => {
-  ctx.body = 'Hello TimeImage'
-})
+router
+  .get('/', ctx => {
+    ctx.body = 'hello koa router'
+  })
+  .get('/mysql', ctx => {
+    ctx.body = 'hello mysql'
+  })
 
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+// 启动
+const port = process.env.APP_SERVER_LISTEN_PORT || 3001
 app.listen(port, () => {
   console.log(`Koa is running in port ${port} successfully!`)
 })
